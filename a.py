a36a25c2611971428bd964c3f73411b817ed20d3
builtQ <45253856+builtQ@users.noreply.github.com>
2018-12-04 19:49:44 +0800

    Update a.py

--- a.py
+++ a.py
@@ -5,7 +5,9 @@ with open ("a.txt") as fp:
 		line=fp.readline()
 		if not line:
 			break
-		if(len(line)-1)==0:#统计空白行
+		if(line.strip)()==0:
+		#if(len(line)-1)==0:#统计空白行
 			blanks+1
+		#print(len(line.strip()))
 		count+=1
 print(count,blanks)
