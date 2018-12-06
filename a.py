c71ba793474379be8d64f3ed5d61f50e0cabadcb
builtQ <45253856+builtQ@users.noreply.github.com>
2018-12-06 17:07:45 +0800

    Update a.py

--- a.py
+++ a.py
@@ -67,3 +67,50 @@ for root,dirs,files in os.walk(root_path):
     print("_"*60)
 print(dir_count-1,"Folders")
 print(file_count,"Files")
+
+#以上所有综合
+import os
+from chardet import detect
+def get_file_info(file_path):
+	with open(file_path,"rb") as fp:
+		encode = detect(fp.read())['encoding']
+		info.write("Encoding:"+str(encode))
+
+	line_count,blank_count = 0,0
+	with open(file_path,'r',encoding=encode) as fp:
+		while True:
+			line = fp.readline()
+			if not line:
+				break
+			line_count+=1
+			if len(line.strip()) == 0:
+				blank_count+=1
+	info.write(str(line_count)+"Lines("+str(blank_count)+"blanks)")
+
+root_path = os.getcwd()
+dir_count,file_count = 0,0.
+info = open("file_info.txt",'w')
+for root,dirs,files in os.walk(root_path):
+	if not os.path.isfile(root):
+		dir_count+=1
+	for f in files:
+		file_path = os.path.join(root,f)
+		if os.path.isfile(file_path):
+			file_count+=1
+		info.write("Filename"+os.path.splitext(f)[0])
+		info.write("Type"+os.path.splitext(f)[1])
+		info.write("Size:"+str(os.path.getsize(file_path))+"Bytes")
+		info.write("Location:"+root)
+		get_file_info(file_path)
+		info.write("-"*60)
+info.write(str(dir_count-1)+"Folders")
+info.write(str(file_count)+"Files")
+
+root_path = os.getcwd()
+offset = len(root_path.split("\\"))
+for root,dirs,files in os.walk(root_path):
+	current_path =  root.split("\\")
+	level = len(current_path) - offset
+	info.write("\t"*level+"\\"+current_path[-1])
+	for f in files:
+		info.write("\t"*(level+1)+f)
